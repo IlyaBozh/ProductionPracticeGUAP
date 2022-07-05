352689dfa8278226d5b7e07b2b9cb202ad2599a4
Ilya <ilia.il.27@mail.ru>
2022-07-05 04:34:21 +0300

    Add requstes models & rename project

---
+++ ProductionPracticeGUAP/Models/Requests/DogRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductionPracticeGUAP.API.Models;
+
+public class DogRequest
+{ 
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Breed { get; set; }
+    public double Weight { get; set; }
+}
