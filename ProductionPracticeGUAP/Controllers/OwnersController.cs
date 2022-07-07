df3314a33d037d6968bcec094137ef8586d06f22
Ilya <ilia.il.27@mail.ru>
2022-07-07 04:45:10 +0300

    Add response models, methods in controllers & endPoints

--- ProductionPracticeGUAP/Controllers/OwnersController.cs
+++ ProductionPracticeGUAP/Controllers/OwnersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductionPracticeGUAP.API.Models;
 
 namespace ProductionPracticeGUAP.Controllers;
 
@@ -6,5 +7,49 @@ namespace ProductionPracticeGUAP.Controllers;
 [Route("[controller]")]
 public class OwnersController : Controller
 {
+    [HttpPost]
+    [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status422UnprocessableEntity)]
+    public ActionResult<int> AddOwner(OwnerRequest dog)
+    {
+        return 1;
+    }
 
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    public ActionResult RemoveOwnerById(int id)
+    {
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    public ActionResult UpdateOwnerById(OwnerRequest dog, int id)
+    {
+        return NoContent();
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(OwnerOutPutResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public ActionResult GetOwnerByUserId(int id)
+    {
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(List<OwnerOutPutAllResponse>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    public ActionResult GetAllOwners()
+    {
+        return NoContent();
+    }
 }
---
+++ ProductionPracticeGUAP/Models/Responses/OwnerOutPutAllResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ProductionPracticeGUAP.API.Models;
+
+public class OwnerOutPutAllResponse
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
