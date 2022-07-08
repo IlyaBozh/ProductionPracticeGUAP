4de01e89795fbaa080bfc92ee6974a2b1d5f21a4
Ilya <ilia.il.27@mail.ru>
2022-07-08 21:24:13 +0300

    Add DataAnatation & ApiErroeMessage

--- ProductionPracticeGUAP/Models/Requests/OwnerRequest.cs
+++ ProductionPracticeGUAP/Models/Requests/OwnerRequest.cs
@@ -1,9 +1,21 @@
-﻿namespace ProductionPracticeGUAP.API.Models;
+﻿using ProductionPracticeGUAP.API.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductionPracticeGUAP.API.Models;
 
 public class OwnerRequest
 {
+    [Required(ErrorMessage = ApiErrorMessage.NameIsRequired)]
+    [MaxLength(30)]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = ApiErrorMessage.LastNameIsRequired)]
+    [MaxLength(30)]
     public string LastName { get; set; }
+
+    [Required(ErrorMessage = ApiErrorMessage.EmailIsRequired)]
     public string Email { get; set; }
+
+    [Required(ErrorMessage = ApiErrorMessage.PasswordIsRequired)]
     public string Password { get; set; }
 }
--- ProductionPracticeGUAP/Models/Requests/OwnerUpdateRequest.cs
+++ ProductionPracticeGUAP/Models/Requests/OwnerUpdateRequest.cs
@@ -1,7 +1,15 @@
-﻿namespace ProductionPracticeGUAP.API.Models;
+﻿using ProductionPracticeGUAP.API.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductionPracticeGUAP.API.Models;
 
 public class OwnerUpdateRequest
 {
+    [Required(ErrorMessage = ApiErrorMessage.NameIsRequired)]
+    [MaxLength(30)]
     public string FirstName { get; set; }
+
+    [Required(ErrorMessage = ApiErrorMessage.LastNameIsRequired)]
+    [MaxLength(30)]
     public string LastName { get; set; }
 }
